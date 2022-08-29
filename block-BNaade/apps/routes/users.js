90beafe5cdb7f49896c0564c1e81f04612149952
jopoepl <joel_joseph93@outlook.com>
2022-08-29 14:44:38 +0530

    CRUD Express Update 2

--- block-BNaade/apps/routes/users.js
+++ block-BNaade/apps/routes/users.js
@@ -1,9 +1,9 @@
+const { application } = require("express")
 var express = require(`express`)
 var router = express.Router()
 var User = require(`../models/user`)
 
 //routes
-
 
 
 router.get(`/new`, (req, res) => {
@@ -51,20 +51,22 @@ router.put(`/:id` ,(req, res, next) => {
     })
 })
 
-router.post(`/:id` ,(req, res, next) => {
-    let id = req.params.id
-    User.findByIdAndUpdate(id, req.body, (err, updatedUsers) => {
-        console.log(updatedUsers)
-
-        res.redirect(`/users`)
-    })
-})
 
 router.get(`/:id` + `/edit`, (req, res, next) => {
     let id = req.params.id
     User.findById(id, (err, user) => {
         if (err) console.log(err)
         res.render(`updateUser`, {user: user})
+    })
+})
+
+router.post(`/:id` ,(req, res, next) => {
+    let id = req.params.id
+    console.log(id)
+    User.findByIdAndUpdate(id, req.body, (err, updatedUsers) => {
+        console.log(updatedUsers)
+
+        res.redirect(`/users/${id}`)
     })
 })
 
